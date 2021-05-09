48937b6bd61e8b4c9cbbf05dec2caadb11aa6029
NiikitaK <82287490+NiikitaK@users.noreply.github.com>
2021-05-09 19:22:15 +0400

    add Sort (POST GET)

--- src/main/java/com/springboot/history/app/model/entity/BuildHistory.java
+++ src/main/java/com/springboot/history/app/model/entity/BuildHistory.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Comparator;
 import java.util.Date;
 
 @Setter
